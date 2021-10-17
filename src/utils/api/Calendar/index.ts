cead666047309d88513e62a9e8db2867ee8deba0
Jun Joy <chojuneseo1@gmail.com>
2021-10-17 23:40:51 +0900

    :gear: :: (#12) Api Moduling

---
+++ src/utils/api/Calendar/index.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const getRequest = () => {
+  const request = axios.create({
+    timeout: 10000,
+    baseURL: "test",
+    withCredentials: true,
+    headers: {
+      withCredentials: true,
+    },
+  });
+
+  return request;
+};
+
+export const getRequestWithToken = (
+  token: string,
+  type: "json" | "blob" | "text" = "json"
+) => {
+  const request = axios.create({
+    timeout: 10000,
+    baseURL: "test",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      withCredentials: true,
+    },
+    responseType: type,
+    withCredentials: true,
+  });
+
+  return request;
+};
