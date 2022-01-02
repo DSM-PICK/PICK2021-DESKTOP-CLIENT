ffc8b2f98b524f1ce74015e270cacaace51b9242
LeeJinWoo <wlsdn5793@gmail.com>
2022-01-02 22:25:26 +0900

    ⚡️ :: (#66) 장소 변경 기능 구현

--- src/components/ClassList/ClassList.tsx
+++ src/components/ClassList/ClassList.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { useLayoutEffect } from "react";
 import { getLocationList } from "../../utils/api/Locationlist";
 import * as S from "./styles";
 const floorArr = ["4층", "3층", "2층", "기타"];
 const ClassList = () => {
-  const [classArr, setClassArr] = useState<any>([]);
   const [afterschool, setAfterschool] = useState<number>(1);
+  const [floor, setFloor] = useState<number>(0);
+
+  useEffect(() => {
+    setFloor(0);
+  }, [afterschool]);
+  useEffect(() => {}, [floor]);
+
   useLayoutEffect(() => {
     const token = localStorage.getItem("access_token");
     if (!token) {
@@ -13,30 +20,41 @@ const ClassList = () => {
     }
     getLocationList(token).then((res) => console.log(res.data));
   }, []);
-  const changeFloor = () => {};
+
+  const changeFloor = (index: number) => {
+    setFloor(index);
+  };
   return (
     <S.BtnContainer>
       <S.ClubState>
         <S.ClassName>GRAM</S.ClassName>
         <span>담당교사:신요신</span>
         <span>총 학생:19명</span>
-        <span>출석 예정:13명</span>
       </S.ClubState>
       <S.ClubLocationWrapper>
         <S.Location>
           <span style={{ color: "black", cursor: "default" }}>층</span>
-          {floorArr.map((floor, i) => (
-            <span key={i} onClick={changeFloor}>
-              {floor}
-            </span>
+          {floorArr.map((floorname, i) => (
+            <S.FloorName
+              key={i}
+              onClick={() => changeFloor(i)}
+              select={floor === i}
+            >
+              {floorname}
+            </S.FloorName>
           ))}
         </S.Location>
-        <S.Location></S.Location>
       </S.ClubLocationWrapper>
       <S.BtnWrapper>
-        <S.BtnItem onClick={() => setAfterschool(1)}>방과후</S.BtnItem>
-        <S.BtnItem onClick={() => setAfterschool(2)}>전공</S.BtnItem>
-        <S.BtnItem onClick={() => setAfterschool(3)}>자습</S.BtnItem>
+        <S.BtnItem onClick={() => setAfterschool(1)} select={afterschool === 1}>
+          방과후
+        </S.BtnItem>
+        <S.BtnItem onClick={() => setAfterschool(2)} select={afterschool === 2}>
+          전공
+        </S.BtnItem>
+        <S.BtnItem onClick={() => setAfterschool(3)} select={afterschool === 3}>
+          자습
+        </S.BtnItem>
       </S.BtnWrapper>
     </S.BtnContainer>
   );
