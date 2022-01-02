21700139220f6798988615856d3c2e5d3878534d
Jun Joy <chojuneseo1@gmail.com>
2022-01-02 16:55:01 +0900

    ⚡️ :: (#35) 타입에 따른 객체 변경

--- src/components/AttendanceChange/ACColumn/Type/index.tsx
+++ src/components/AttendanceChange/ACColumn/Type/index.tsx
@@ -2,11 +2,15 @@ import React, { FC, useEffect, useRef, useState } from "react";
 import * as S from "./styles";
 import { COLOR } from "../../../../style";
 import { ACColumn } from "../../styles";
+import { TypeIndex as TypeAtom, StudentObject as StudentAtom, SelectedIndex } from "../../../../state/atom/ATChange";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 const AttendanceChange: FC = (): JSX.Element => {
   const ACListArray: string[] = ["결석일", "결석자", "종류", "신고자", "비고"];
   const TypesArray: string[] = ["외출", "현체", "귀가", "이동", "취업"];
-  const [typeIndex, setTypeIndex] = useState<number>(0);
+  const [typeIndex, setTypeIndex] = useRecoilState<number>(TypeAtom);
+  const setStudentObject = useSetRecoilState(StudentAtom);
+  const selectedIndex = useRecoilValue(SelectedIndex);
   const TypesRefs = useRef(new Array(TypesArray.length));
   const TestArray = [];
 
@@ -19,13 +23,20 @@ const AttendanceChange: FC = (): JSX.Element => {
   };
 
   useEffect(() => {
+    console.log(selectedIndex)
     for (let i = 0; i < ACListArray.length; i++) {
       TypesRefs.current[i].style.backgroundColor = `${COLOR.white}`;
       TypesRefs.current[i].style.color = `${COLOR.black}`;
     }
     TypesRefs.current[typeIndex].style.backgroundColor = `${COLOR.orange}`;
     TypesRefs.current[typeIndex].style.color = `${COLOR.white}`;
+
+    setStudentObject((prevArr) => prevArr.map((value) => {
+      return value.id === selectedIndex ? { ...value, type: typeIndex } : value;
+    }))
   }, [typeIndex]);
+
+  
 
   return (
     <ACColumn>
