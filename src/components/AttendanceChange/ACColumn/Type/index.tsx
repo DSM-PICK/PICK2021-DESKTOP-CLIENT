82290b532eb5a675c7201bec5e8c6a717d55a903
Jun Joy <chojuneseo1@gmail.com>
2022-01-02 21:45:11 +0900

    ⚡️ :: (#35) 출결변경 날 타입 변경 완료

--- src/components/AttendanceChange/ACColumn/Type/index.tsx
+++ src/components/AttendanceChange/ACColumn/Type/index.tsx
@@ -1,42 +1,55 @@
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useEffect, useLayoutEffect, useRef, useState } from "react";
 import * as S from "./styles";
 import { COLOR } from "../../../../style";
 import { ACColumn } from "../../styles";
-import { TypeIndex as TypeAtom, StudentObject as StudentAtom, SelectedIndex } from "../../../../state/atom/ATChange";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  TypeIndex as TypeAtom,
+  StudentObject as StudentAtom,
+  SelectedIndex,
+} from "../../../../state/atom/ATChange";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 const AttendanceChange: FC = (): JSX.Element => {
   const ACListArray: string[] = ["결석일", "결석자", "종류", "신고자", "비고"];
   const TypesArray: string[] = ["외출", "현체", "귀가", "이동", "취업"];
   const [typeIndex, setTypeIndex] = useRecoilState<number>(TypeAtom);
-  const setStudentObject = useSetRecoilState(StudentAtom);
+  const [studentObject, setStudentObject] = useRecoilState<any>(StudentAtom);
   const selectedIndex = useRecoilValue(SelectedIndex);
   const TypesRefs = useRef(new Array(TypesArray.length));
-  const TestArray = [];
-
-  for (let i = 0; i < 20; i++) {
-    TestArray.push(i);
-  }
 
   const changeTypeIndex = (index: number) => {
     setTypeIndex(index);
   };
 
   useEffect(() => {
-    console.log(selectedIndex)
+    if (studentObject.length === 0) return;
+
+    setStudentObject(
+      studentObject.length === 0
+        ? studentObject
+        : (prevArr: any) =>
+            prevArr.map((value: any) => {
+              return value.id === selectedIndex
+                ? { ...value, type: typeIndex }
+                : value;
+            })
+    );
+
     for (let i = 0; i < ACListArray.length; i++) {
       TypesRefs.current[i].style.backgroundColor = `${COLOR.white}`;
       TypesRefs.current[i].style.color = `${COLOR.black}`;
     }
     TypesRefs.current[typeIndex].style.backgroundColor = `${COLOR.orange}`;
     TypesRefs.current[typeIndex].style.color = `${COLOR.white}`;
-
-    setStudentObject((prevArr) => prevArr.map((value) => {
-      return value.id === selectedIndex ? { ...value, type: typeIndex } : value;
-    }))
   }, [typeIndex]);
 
-  
+  useEffect(() => {
+    setTypeIndex(
+      studentObject.length === 0
+        ? 0
+        : studentObject.find((value: any) => value.id === selectedIndex).type
+    );
+  }, [selectedIndex]);
 
   return (
     <ACColumn>
